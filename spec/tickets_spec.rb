cddab94bae870a9fe502a79e83dc42aca1f0ced9
Omar Rodriguez <omarjavier15@gmail.com>
2012-04-04 11:13:03 +0000

    example to test update ticket functionality added to Ticket spec

--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -18,77 +18,83 @@ describe "Ticketmaster::Provider::Pivotal::Ticket" do
     @project_id = 93790
     @ticket_id = 4056827
   end
-  
+
   before(:each) do
     @ticketmaster = TicketMaster.new(:pivotal, :token => '000000')
     @project = @ticketmaster.project(@project_id)
     @klass = TicketMaster::Provider::Pivotal::Ticket
   end
-  
+
   it "should be able to load all tickets" do
     @project.tickets.should be_an_instance_of(Array)
     @project.tickets.first.should be_an_instance_of(@klass)
   end
-  
+
   it "should be able to load all tickets based on an array of ids" do
     @tickets = @project.tickets([@ticket_id])
     @tickets.should be_an_instance_of(Array)
     @tickets.first.should be_an_instance_of(@klass)
     @tickets.first.id.should == @ticket_id
   end
-  
+
   it "should be able to load all tickets based on attributes" do
     @tickets = @project.tickets(:id => @ticket_id)
     @tickets.should be_an_instance_of(Array)
     @tickets.first.should be_an_instance_of(@klass)
     @tickets.first.id.should == @ticket_id
   end
-  
+
   it "should return the ticket class" do
     @project.ticket.should == @klass
   end
-  
+
   it "should be able to load a single ticket" do
     @ticket = @project.ticket(@ticket_id)
     @ticket.should be_an_instance_of(@klass)
     @ticket.id.should == @ticket_id
   end
-  
+
   it "should be able to load a single ticket based on attributes" do
     @ticket = @project.ticket(:id => @ticket_id)
     @ticket.should be_an_instance_of(@klass)
     @ticket.id.should == @ticket_id
   end
-  
+
   it "should be able to update and save a ticket" do
     @ticket = @project.ticket(@ticket_id)
     #@ticket.save.should == nil
     @ticket.description = 'hello'
     @ticket.save.should == true
   end
-  
+
   it "should be able to create a ticket" do
     @ticket = @project.ticket!(:title => 'Ticket #12', :description => 'Body')
     @ticket.should be_an_instance_of(@klass)
   end
 
-  it "should be able to load all tickets based on attributes using updated_at field" do 
+  it "should be able to load all tickets based on attributes using updated_at field" do
     @ticket = @project.ticket(@ticket_id)
     tickets = @project.tickets(:updated_at => @ticket.updated_at)
     tickets.should be_an_instance_of(Array)
     tickets.first.should be_an_instance_of(@klass)
   end
 
-  it "shoule be able to load all tickets based on attributes using created_at field" do 
+  it "shoule be able to load all tickets based on attributes using created_at field" do
     @ticket = @project.ticket(@ticket_id)
     tickets = @project.tickets(:created_at => @ticket.created_at)
     tickets.should be_an_instance_of(Array)
     tickets.first.should be_an_instance_of(@klass)
   end
 
-  it "should return the requested_by field" do 
+  it "should return the requested_by field" do
     @ticket = @project.ticket(@ticket_id)
     @ticket.requestor.should == 'Hong Quach'
   end
-  
+
+  it "should be able to update a ticket" do
+    @ticket = @project.ticket(@ticket_id)
+    @ticket.title = "Hello World"
+    @ticket.save.should be_true
+  end
+
 end
